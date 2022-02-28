5f6038615ba865b72ea2de5feb28c26f80506d64
dom <domlambwebdev@gmail.com>
2022-02-28 07:55:30 -0500

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// let name: any;
+// let age: number | string;
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number, string];
+
+// let printName: (name: string) => never;
+
+// let personName: unknown;
+
+// interface Person {
+// 	name: string;
+// 	age?: number;
+// }
+
+// interface Guy extends Person {
+// 	profession: string;
+// }
+
+// type X = {
+// 	a: string;
+// 	b: number;
+// };
+
+// type Y = X & {
+// 	c: string;
+// 	d: number;
+// };
+
+// missing a, b
+// let y: Y = {
+// 	c: 'efd',
+// 	d: 42,
+// };
+
+// function printName(name: string) {
+// 	console.log(name);
+// }
+
+// printName('dom');
+// role = [10, 'tim'];
+
+// type Person = {
+// 	name: string;
+// 	age?: number;
+// };
+
+// let person: Person = {
+// 	name: 'Dom',
+// };
+
+// let lotsOfPeople: Person[];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">Hello World.</header>
+		</div>
+	);
 }
 
 export default App;
